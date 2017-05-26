31451b9f459dd4e0af3a9c9c32c0431eac9b4e1c
JohnGiorgi <johnmgiorgi@gmail.com>
2017-05-26 16:11:40 -0700

    Added entity_template.yml. Auto-generated rules now working

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 from collections import Counter
 
 def character_ngrams(string, n):
+
 	ngrams = []
 	string = "^" + string + "$"
 	
@@ -19,10 +20,20 @@ def ngrams_from_file(file_path, nlp_pipeline, n):
 	cnts = Counter()
 	for distinct_line in distinct_lines:
 		doc = nlp_pipeline.bionlp.annotate(distinct_line)
-		counts_for_doc = ngrams_from_document(doc, n)
+		counts_for_doc = ngrams_from_doc(doc, n)
 		for k,v in counts_for_doc.items():
 			cnts[k] += v
 	
+	# normalize the dictionary
+	return normalize(cnts)
+
+def ngrams_from_documents(documents, n):
+	
+	cnts = Counter()
+	for doc in documents:
+		counts_for_doc = ngrams_from_doc(doc, n)
+		for k,v in counts_for_doc.items():
+			cnts[k] += v
 	# normalize the dictionary
 	return normalize(cnts)
 
@@ -30,7 +41,7 @@ def normalize(cnts_dict):
 	total = sum(v for k,v in cnts_dict.items())
 	return Counter({k: v / total for k,v in cnts_dict.items()})
 
-def ngrams_from_document(doc, n):
+def ngrams_from_doc(doc, n):
 
 	'''
 	Takes an annotated processors.ds.Document 
@@ -42,3 +53,33 @@ def ngrams_from_document(doc, n):
 			for gram in ngrams:
 				cnt[gram] += 1
 	return cnt
+
+def diff_distributions(target_dist, background_dist):
+	for k,v in background_dist.items():
+		target_value = target_dist[k]
+		if target_value > 0:
+			difference = target_value - v
+			target_dist[k] = difference if difference > 0 else 0
+	# now normalize target
+	return normalize(target_dist)
+
+def create_morphological_rule(ngram_dist, label, top_n):
+	"""
+	Gus: instantiates a rule template from a character ngram distribution.
+	"""
+	sub_patterns = [k for k,v in ngram_dist.most_common(top_n)]
+	rules = []
+	for i, sub_pattern in enumerate(sub_patterns):
+		with open("entity_template.yml") as in_file:
+			rule_template = in_file.read()
+		# this way we can evaluate the feature quality of each ngram from our distribution
+		rule_name = "{}-{}".format(label.lower(), i + 1)
+		rule = rule_template.format(
+			name=rule_name,
+			label=label,
+			pattern="""[tag=/^NN/ & lemma=/{p}/]""".format(p=sub_pattern)
+		)
+		rules.append(rule)
+	return "\n\n".join(rules)
+
+
